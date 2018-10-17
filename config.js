dea4c97c36fb07ca9462b4a29744d2835b4514c7
Sneha Munden <snehamunden@gmail.com>
2018-10-17 12:33:54 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+var config = {
+                'invokeUrl': 'https://aschc0r9ke.execute-api.us-east-1.amazonaws.com/chatbotdeploy/chatbot',
+                'apiKey': 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+                'region': 'us-east-1' 
+              }
