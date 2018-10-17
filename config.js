98a1e63e3b814a9dc0c187d705b1d27c44eb72ee
Sneha Munden <snehamunden@gmail.com>
2018-10-17 12:28:43 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {
                 'invokeUrl': 'https://aschc0r9ke.execute-api.us-east-1.amazonaws.com/chatbotdeploy/chatbot',
-                'apiKey': 'ZQk3Eo481U2YXo8ONBBcU6SnXlzzsQwh58VlfjCh',
+                'apiKey': 'xxxxxxxxxxxxxxxxxxxxxxx',
                 'region': 'us-east-1' 
               }
