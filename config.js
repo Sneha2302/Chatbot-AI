96e522fd61ab4315f66fa96c9426ae96400ac241
Sneha Munden <snehamunden@gmail.com>
2018-10-17 12:33:28 -0400

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-var config = {
-                'invokeUrl': 'https://aschc0r9ke.execute-api.us-east-1.amazonaws.com/chatbotdeploy/chatbot',
-                'apiKey': 'xxxxxxxxxxxxxxxxxxxxxxx',
-                'region': 'us-east-1' 
-              }
